a50585a3bf0169ff3d41398ee970ca5a31bc3515
nguyenngocminhthu <67693186+nguyenngocminhthu@users.noreply.github.com>
2021-11-30 12:39:03 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require("cors");
-const dotenv= require("dotenv")
+const dotenv = require("dotenv")
 const logger = require("morgan")
 const passport = require("passport")
 const session = require("express-session")
@@ -18,7 +18,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-}),cors())
+}), cors())
 
 dotenv.config()
 
